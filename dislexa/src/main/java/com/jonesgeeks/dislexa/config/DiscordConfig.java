4eef8a43aac374a65c3997ed9957112e67e30806
William Jones <will@jonesgeeks.com>
2017-05-06 17:55:35 -0600

    Added dislexa svg

--- dislexa/src/main/java/com/jonesgeeks/dislexa/config/DiscordConfig.java
+++ dislexa/src/main/java/com/jonesgeeks/dislexa/config/DiscordConfig.java
@@ -27,10 +27,8 @@ import ai.kitt.snowboy.SnowboyDetect;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
-import net.dv8tion.jda.core.hooks.EventListener;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 /**
