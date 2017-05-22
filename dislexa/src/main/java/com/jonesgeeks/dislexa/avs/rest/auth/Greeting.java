8b8f352be86c35f7d87d2753647d6a53e700198b
William Jones <will@jonesgeeks.com>
2017-05-22 16:59:46 -0600

    Added sample rest controller

---
+++ dislexa/src/main/java/com/jonesgeeks/dislexa/avs/rest/auth/Greeting.java
@@ -0,0 +1,19 @@
+package com.jonesgeeks.dislexa.avs.rest.auth;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
